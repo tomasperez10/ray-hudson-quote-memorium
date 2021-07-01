9e595c4b0c33383775e9dda1a16cc7043fc7994c
Tomas Perez <tomasitopg6@gmail.com>
2021-07-01 04:27:14 -0400

    edited routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :quotes
-  resources :users
+  resources :users do
+    resources :quotes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
