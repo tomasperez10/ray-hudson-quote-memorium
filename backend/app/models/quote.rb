54c1b9b41878abdaaa9e030773c13c5eee899b83
Tomas Perez <tomasitopg6@gmail.com>
2021-07-01 04:35:43 -0400

    adding methods to models

--- backend/app/models/quote.rb
+++ backend/app/models/quote.rb
@@ -1,3 +1,13 @@
 class Quote < ApplicationRecord
+    include ActiveModel::Serializers::JSON
+
     belongs_to :user
+
+    def self.current_user 
+        current_user = User.last
+    end
+
+    def attributes
+        {"username" => nil, "id" => nil, "quotes" => {}}
+    end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+    include ActiveModel::Serializers::JSON
+
     has_many :quotes
+
+    def attributes
+        {"description" => nil, "id" => nil}
+    end
 end
