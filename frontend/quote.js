da1ee309701418f0841e55e6cdc664b82ab206c8
Tomas Perez <tomasitopg6@gmail.com>
2021-07-02 03:46:44 -0400

    adding script sources to html file

--- frontend/quote.js
+++ frontend/quote.js
@@ -5,11 +5,12 @@ class Quote{
         this.id = quote.id
     }
 
-    static all = []
+    static allQuotes = []
     static quote1 = "You can follow him on Facebook, and he'll come out on Twitter!"
     static quote2 = "Sir Isaac Newton from up above, looks down and says, 'About that gravity thing? I was wrong, Messi was right!'"
     static quote3 = "You can drop a tarantula into his shorts and he'll still be cool!"
     static quote4 = "He curls this divinely with more curve on it than Jessica Rabbit!"
     static quote5 = "As electrifying as a hairdryer thrown into a hot tub!"
+
     
 }
