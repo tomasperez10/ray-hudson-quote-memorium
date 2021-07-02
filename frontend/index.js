d00242992913eb3c9e971950a7dfa7a046542853
Tomas Perez <tomasitopg6@gmail.com>
2021-07-02 00:12:56 -0400

    adding variables for html elements

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+const quoteContainer = document.getElementById("quote-container")
+const buttonQuote = document.getElementById("find-quotes")
+const backgroundImg = document.body.style.backgroundImage = "images/ray-hudson.jpeg";
+const username = document.getElementById('user-name')
+
+
+
+
+buttonQuote.addEventListener("click", User.getUsername)
