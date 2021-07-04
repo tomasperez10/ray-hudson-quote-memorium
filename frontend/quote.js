9310750814760d01e63d8e93601f872b3a57452b
Tomas Perez <tomasitopg6@gmail.com>
2021-07-04 03:06:23 -0400

    creates, adds, and fetches quotes

--- frontend/quote.js
+++ frontend/quote.js
@@ -1,3 +1,7 @@
+const quotesDiv = document.getElementById("quote-container")
+let div = document.getElementById("single-quote")
+
+
 class Quote{
     constructor(quote){
         Quote.allQuotes.push(this)
@@ -6,6 +10,35 @@ class Quote{
     }
 
     static allQuotes = []
+    static counter = 0
 
-    
+    static createQuotes(quotes){
+        for (let q of quotes){
+            new Quote(q) 
+        }
+        const q = Quote.allQuotes[Quote.counter++]
+        q.appendQuote()
+    };
+
+    appendQuote(){
+        if (!!alertMess){
+            alertMess.remove()
+        }
+        if (!div){
+            div = document.createElement("div")
+            div.setAttribute('id', 'single-quote')
+        }
+        const h2 = document.createElement('h2')
+        h2.innerText = this.description
+        div.append(h2)
+        quotesDiv.append(div)
+    };
+
+    static fetchQuotes(){
+        username.remove()
+        buttonQuote.remove()
+        fetch("http://localhost:3000/quotes")
+        .then(resp => resp.json())
+        .then(Quote.createQuotes)
+    }
 }
