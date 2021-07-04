682d8f7c43d90beb2918b0a7844d81af5789ddab
Tomas Perez <tomasitopg6@gmail.com>
2021-07-04 02:45:06 -0400

    seeding data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quote.create(description: "You can follow him on Facebook, and he'll come out on Twitter!")
+
+Quote.create(description: "Sir Isaac Newton from up above, looks down and says, 'About that gravity thing? I was wrong, Messi was right!'")
+
+Quote.create(description: "You can drop a tarantula into his shorts and he'll still be cool!")
+
+Quote.create(description: "He curls this divinely with more curve on it than Jessica Rabbit!")
+
+Quote.create(description: "As electrifying as a hairdryer thrown into a hot tub!")
+
