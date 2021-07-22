5ef52f867e868c21d2a2c6769065a92a178af227
Tomas Perez <tomasitopg6@gmail.com>
2021-07-22 05:29:29 -0400

    removing unneccessary code

--- public/src/quote.js
+++ public/src/quote.js
@@ -2,11 +2,11 @@ const quotesDiv = document.getElementById("quote-container")
 const headDiv = document.getElementById("quote-container")
 
 class Quote {
-    constructor(quotes){
-        this.description = quotes.description
-        this.link = quotes.link
-        this.id = quotes.id
-    }
+    // constructor(quotes){
+    //     this.description = quotes.description
+    //     this.link = quotes.link
+    //     this.id = quotes.id
+    // }
 
     static allQuotes = [{description: "You can follow him on Facebook, and he'll come out on Twitter!", link: "https://www.youtube.com/watch?v=oT9kPvzYPhs"},
     {description: "Sir Isaac Newton from up above, looks down and says, 'About that gravity thing? I was wrong, Messi was right!'", link: "https://www.youtube.com/watch?v=ciQKYsXip-M"},
@@ -14,7 +14,6 @@ class Quote {
     {description: "He curls this divinely with more curve on it than Jessica Rabbit!", link: "https://www.youtube.com/watch?v=jQGIceRIzuA"},
     {description: "As electrifying as a hairdryer thrown into a hot tub!", link: "https://www.youtube.com/watch?v=0Pca2t3A5rk"}]
 
-    static counter = 0
 
     static createQuotes(){
         for (let quotes of Quote.allQuotes) {
@@ -31,7 +30,10 @@ class Quote {
                 h2.innerHTML = a.title
                 div.appendChild(a)
                 quotesDiv.append(div)
-                return fetch(`http://localhost:3000/quotes/${quotes.link}`)
+                return fetch(`http://localhost:3000/quotes/${quotes.link}`,
+                {
+                    method: "GET"
+                })
                 .then(resp => resp.json)
                 .then(quotes)
                 // .then(Quote.dynamicVideo)
@@ -50,29 +52,15 @@ class Quote {
         
     };
 
-    // static dynamicVideo(){
-    //     let ytVideos = JSON.parse(Quote.allQuotes)
-
-    //     for (let i in ytVideos.quotes) {
-    //         let ytVideo = $("<iframe/>");
-    //         ytVideo.attr({
-    //           width: 560,
-    //           height: 315,
-    //           src: 'https://www.youtube.com/embed/' + ytVideos.quotes[i].link,
-    //           frameborder: 0
-    //         });
-    //         $("#quote-container").append(ytVideo);
-    //     }
-    // }
-
-    
-
     static fetchQuotes(){
         username.remove()
         buttonQuote.remove()
         welcome.remove()
         headDiv.append("Our 5 best Ray Hudson quotes! Click the quotes to see his quotes in action on camera!")
-        fetch("http://localhost:3000/quotes")
+        fetch("http://localhost:3000/quotes",
+        {
+            method: "GET"
+        })
         .then(resp => resp.json())
         .then(data => console.log(data))
         .then(Quote.createQuotes)
